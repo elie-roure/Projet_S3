90b237ed0faec7d554e6986b4f5b5b1598f12ff7
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-10-17 21:21:45 +0200

    commit initial

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,28 +1,19 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        primaryStage.setTitle("MAP");
-
-        Group root = new Group();
-        Scene scene = new Scene(root, 500, 500, Color.WHITE);
-
-        MapProcedurale map = new MapProcedurale(20,20, 6);
-        root.getChildren().add(map);
-
-        primaryStage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-
     }
 
 
